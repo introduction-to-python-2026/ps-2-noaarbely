4c0c6b6dbe797d081627cec7c8365806859313cf
noaarbely <noa.arbely@gmail.com>
2025-10-31 16:04:16 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,3 +1,11 @@
+def find_max_number (num1, num2, num3):
+   if num1>=num2 and num1>=num3:
+      return num1
+   elif num2>=num1 and num2>=num1:
+      return num2
+   else:
+      return num3
+
 def find_mean_std (x1, x2, x3):
     mean = (x1 + x2 +x3) / 3
     variance_std= (((x1-mean)**2 + (x2-mean)**2 + (x3-mean)**2) / 3)**0.5
